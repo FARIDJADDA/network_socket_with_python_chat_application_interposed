dbca1dfa0ade084543b396ef569d9927f8e133ef
Farid Djadda <djaddafarid.contact@gmail.com>
2023-07-25 15:59:12 +0200

    Amélioration du client: réessayer la connexion au serveur

--- 2/sockets_client.py
+++ 2/sockets_client.py
@@ -1,16 +1,25 @@
 
 import socket
+import time
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 
-s = socket.socket()
+
 
 print(f"Connexion au serveur {HOST_IP}, port {HOST_PORT}")
-try: 
-    s.connect((HOST_IP, HOST_PORT))
-except ConnectionRefusedError:
-    print("ERREUR: Le server n'a pas réussit à ce connecter")
-else: 
-    print("Connecté au serveur")
-    s.close()
+while True:
+    try: 
+        s = socket.socket()
+        s.connect((HOST_IP, HOST_PORT))
+    except ConnectionRefusedError:
+        print("ERREUR: Impossible de se connecter au server. Reconnexion en cours...")
+        time.sleep(5)
+    else: 
+        print("Connecté au serveur")
+    break
+
+# ....... utilisation de la socket
+
+
+s.close()
